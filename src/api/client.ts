a71c36c16a0dd42320a8bb7d952ec1f42d486e30
vanadium5000 <vanadium5000@gmail.com>
2025-10-28 00:36:09 +0000

    testing

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import { Api } from "./api";
 export const api = new Api({
   baseUrl:
     import.meta.env.MODE === "production"
-      ? "https://my-website.space/backend/"
+      ? "https://my-website.space/backend"
       : "http://localhost:3000",
   securityWorker: () => ({
     // headers: {
