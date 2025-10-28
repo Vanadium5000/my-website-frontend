92f6a62f35cb731ce9f85065b16785d7296be24d
vanadium5000 <vanadium5000@gmail.com>
2025-10-28 02:41:39 +0000

    Fix?

--- default.nix
+++ default.nix
@@ -1,17 +1,13 @@
 # default.nix
 { stdenv }:
-
 stdenv.mkDerivation {
   pname = "frontend-package";
   version = "1.0";
-
   src = ./.;
-
   dontUnpack = true;
   dontBuild = true;
-
   installPhase = ''
     mkdir -p $out
-    cp -r $src/dist/* $out/
+    cp -r $src/dist/* $out/ || true
   '';
 }
